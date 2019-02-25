790997f14cfe5b361f4a0752c76b1d472fbfdfc3
Igor Nemykin <webroru@gmail.com>
2019-02-25 22:45:38 +0300

    add mailchecher

--- Parser.php
+++ Parser.php
@@ -8,7 +8,6 @@ class Parser
 
     public function __construct(string $mail)
     {
-        //$this->mail = new \SimpleXMLElement($mail);
         $dom = new \DomDocument();
         $dom->loadHTML($mail);
         $this->mail = new \DOMXpath($dom);
@@ -16,37 +15,32 @@ class Parser
 
     public function getCheckInDate()
     {
-        $elements = $this->mail->query('//table/tbody/tr[6]/td[2]/b');
+        $elements = $this->mail->query('//table/tr[6]/td[2]/b');
 
         return $elements ? $elements[0]->nodeValue : null;
-
     }
 
     public function getCheckOutDate()
     {
-        $elements = $this->mail->query('//table/tbody/tr[7]/td[2]/b');
-
+        $elements = $this->mail->query('.//table/tr[7]/td[2]/b');
         return $elements ? $elements[0]->nodeValue : null;
     }
 
     public function getBookingNumber()
     {
-        $elements = $this->mail->query('//table/tbody/tr[5]/td[2]');
+        $elements = $this->mail->query('//table//tr[5]/td[2]');
         if (!$elements) {
             return null;
         }
-        return preg_replace('/[\D]+/', '', $elements[0]->nodeValue);
+        preg_match('/new ([\d]+)/', $elements[0]->nodeValue, $matches);
+        //return preg_replace('/[\D]+/', '', $elements[0]->nodeValue);
+        return $matches[1];
     }
 
     public function getGuestName()
     {
-        $elements = $this->mail->query('//table/tbody/tr[14]/td[2]/b');
+        $elements = $this->mail->query('//table/tr[13]/td[2]/b');
 
         return $elements ? $elements[0]->nodeValue : null;
-    }
-
-    public function getRoomNumber()
-    {
-        return 1;
     }
 }
--- app.php
+++ app.php
@@ -1,21 +1,24 @@
 <?php
 
+use App\MailChecker;
 use App\Parser;
 use App\ScienerApi;
 
 require __DIR__ . '/vendor/autoload.php';
 
-$mail = file_get_contents('./mail.html');
-
-$parser = new Parser($mail);
+$mailChecker = new MailChecker();
 $scienerApi = new ScienerApi();
 
-$checkInDate = $parser->getCheckInDate();
-$checkOutDate = $parser->getCheckOutDate();
-$bookingNumber = $parser->getBookingNumber();
-$guestName = $parser->getGuestName();
-$password = substr($bookingNumber, -6);
-$checkInDateInMs = (new \DateTime($checkInDate))->getTimestamp() * 1000;
-$checkOutDateInMs = (new \DateTime($checkOutDate))->getTimestamp() * 1000;
-
-var_dump($scienerApi->addPasscode($guestName, $password, $checkInDateInMs, $checkOutDateInMs));
+foreach ($mailChecker->getMail() as $mail) {
+    $parser = new Parser($mail);
+    $checkInDate = $parser->getCheckInDate();
+    $checkOutDate = $parser->getCheckOutDate();
+    $bookingNumber = $parser->getBookingNumber();
+    $guestName = $parser->getGuestName();
+    $password = substr($bookingNumber, -6);
+    $checkInDateInMs = (new \DateTime($checkInDate))->setTime(12, 0)->getTimestamp() * 1000;
+    $checkOutDateInMs = (new \DateTime($checkOutDate))->setTime(12, 0)->getTimestamp() * 1000;
+    if ($scienerApi->addPasscode($guestName, $password, $checkInDateInMs, $checkOutDateInMs)) {
+        echo "For $guestName have been added password: $password valid from $checkInDate to $checkOutDate";
+    }
+}

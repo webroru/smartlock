4abf4367e6a31a1788aa48083ab17ad95fb63b28
Igor Nemykin <webroru@gmail.com>
2019-02-28 10:27:38 +0300

    add try/catch, types

--- Parser.php
+++ Parser.php
@@ -13,27 +13,27 @@ class Parser
         $this->mail = new \DOMXpath($dom);
     }
 
-    public function getCheckInDate()
+    public function getCheckInDate(): ?string
     {
         $elements = $this->mail->query('//table/tr[6]/td[2]/b');
 
         return $elements ? $elements[0]->nodeValue : null;
     }
 
-    public function getCheckOutDate()
+    public function getCheckOutDate(): ?string
     {
         $elements = $this->mail->query('.//table/tr[7]/td[2]/b');
         return $elements ? $elements[0]->nodeValue : null;
     }
 
-    public function getGuestName()
+    public function getGuestName(): ?string
     {
         $elements = $this->mail->query('//table/tr[13]/td[2]/b');
 
         return $elements ? $elements[0]->nodeValue : null;
     }
 
-    public function getPhone()
+    public function getPhone(): ?string
     {
         $elements = $this->mail->query('//table/tr[14]/td[2]/b');
 
--- app.php
+++ app.php
@@ -6,20 +6,37 @@ use App\ScienerApi;
 
 require __DIR__ . '/vendor/autoload.php';
 
-$mailChecker = new MailChecker();
-$scienerApi = new ScienerApi();
+try {
+    run();
+} catch (\Exception $e) {
+    log("Error: {$e->getMessage()}");
+}
 
-foreach ($mailChecker->getMail() as $mail) {
-    $parser = new Parser($mail);
-    $checkInDate = $parser->getCheckInDate();
-    $checkOutDate = $parser->getCheckOutDate();
-    $guestName = $parser->getGuestName();
-    $phone = $parser->getPhone();
-    $password = substr(str_replace(' ', '', $phone), -5);
-    $checkInDateInMs = (new \DateTime($checkInDate))->setTime(12, 0)->getTimestamp() * 1000;
-    $checkOutDateInMs = (new \DateTime($checkOutDate))->setTime(12, 0)->getTimestamp() * 1000;
-    if ($scienerApi->addPasscode($guestName, $password, $checkInDateInMs, $checkOutDateInMs)) {
-        $date = (new \DateTime())->format('Y-m-d h:i:s');
-        echo "$date For $guestName have been added password: $password valid from $checkInDate to $checkOutDate\n";
+function run() {
+    $mailChecker = new MailChecker();
+    $scienerApi = new ScienerApi();
+
+    foreach ($mailChecker->getMail() as $mail) {
+        $parser = new Parser($mail);
+        $checkInDate = $parser->getCheckInDate();
+        $checkOutDate = $parser->getCheckOutDate();
+        $guestName = $parser->getGuestName();
+        $phone = $parser->getPhone();
+        $password = (string) substr(str_replace(' ', '', $phone), -5);
+        if ($scienerApi->addPasscode($guestName, $password, prepareDate($checkInDate), prepareDate($checkOutDate))) {
+            log("For $guestName have been added password: $password valid from $checkInDate to $checkOutDate");
+        }
     }
+}
+
+function prepareDate(string $date): int {
+    return (new \DateTime($date))
+        ->setTime(12, 0)
+        ->setTimezone(new DateTimeZone('Europe/Vienna'))
+        ->getTimestamp() * 1000;
+}
+
+function log(string $message) {
+    $date = (new \DateTime())->format('Y-m-d h:i:s');
+    echo "$date $message\n";
 }

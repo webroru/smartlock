1571dede304e2927d4d64543e47228663aba641e
Igor Nemykin <webroru@gmail.com>
2020-02-02 09:49:25 +0300

    add order entity

---
+++ Order.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App;
+
+class Order
+{
+    private $id;
+    private $name;
+    private $checkInDate;
+    private $checkOutDate;
+    private $email;
+
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    public function setId($id): self
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName($name): self
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    public function getCheckInDate(): \DateTime
+    {
+        return new \DateTime($this->checkInDate);
+    }
+
+    public function setCheckInDate(\DateTime $checkInDate): self
+    {
+        $this->checkInDate = $checkInDate->getTimestamp();
+        return $this;
+    }
+
+    public function getCheckOutDate(): \DateTime
+    {
+        return new \DateTime($this->checkOutDate);
+    }
+
+    public function setCheckOutDate($checkOutDate): self
+    {
+        $this->checkOutDate = $checkOutDate;
+        return $this;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function setEmail($email): self
+    {
+        $this->email = $email;
+        return $this;
+    }
+}

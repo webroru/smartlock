ad28b67881b91700ded238c1f5a07cfe323f76cb
Igor Nemykin <i.nemykin@platform.live>
2022-04-27 09:25:26 +0200

    we do not need to save the Booking entity

--- App/Services/LockService.php
+++ App/Services/LockService.php
@@ -11,24 +11,16 @@ use App\ScienerApi;
 
 class LockService
 {
-    private const REGISTRATION_DELAY = 60 * 60 * 24 * 7; // 1 week
-
     private $scienerApi;
-    private $bookingRepository;
 
     public function __construct(
-        ScienerApi $scienerApi,
-        RepositoryInterface $bookingRepository
+        ScienerApi $scienerApi
     ) {
         $this->scienerApi = $scienerApi;
-        $this->bookingRepository = $bookingRepository;
     }
 
     public function registerBooking(Booking $booking): void
     {
-        if ($booking->getCheckInDate()->getTimestamp() - time() > self::REGISTRATION_DELAY) {
-            $this->lockService->registerBooking($booking);
-        }
         $password = $this->scienerApi->addRandomPasscode(
             $booking->getName(),
             $booking->getCheckInDate()->getTimestamp() * 1000,
@@ -44,7 +36,6 @@ class LockService
             throw new \Exception($error);
         }
         $booking->setCode($password);
-        $this->bookingRepository->update($booking);
 
         //$this->sendMail($booking, $isChanged);
         Logger::log(
@@ -52,14 +43,5 @@ class LockService
             "{$booking->getCheckInDate()->format('Y-m-d H:i')} " .
             "to {$booking->getCheckOutDate()->format('Y-m-d H:i')}"
         );
-    }
-
-    public function removeOldBooking(string $orderId): void
-    {
-        /** @var Booking[] $bookings */
-        $bookings = $this->bookingRepository->findBy(['OrderId' => $orderId]);
-        foreach ($bookings as $booking) {
-            $this->bookingRepository->delete($booking->getId());
-        }
     }
 }
--- index.php
+++ index.php
@@ -3,7 +3,6 @@
 declare(strict_types=1);
 
 use App\Controller\ApiController;
-use App\Repository\BookingMysqlRepository;
 use App\ScienerApi;
 use App\Services\LockService;
 use Symfony\Component\HttpFoundation\Request;
@@ -11,8 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 require __DIR__ . '/bootstrap.php';
 
 $scienerApi = new ScienerApi();
-$bookingRepository = new BookingMysqlRepository();
-$lockService = new LockService($scienerApi, $bookingRepository);
+$lockService = new LockService($scienerApi);
 
 $request = Request::createFromGlobals();
 $apiControler = new ApiController($lockService);

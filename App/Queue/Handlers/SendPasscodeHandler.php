89017d26a69dd4efa60024411bcae608a25d1fd3
Igor Nemykin <i.nemykin@devcaz.com>
2022-10-15 14:33:38 +0200

    update SendPasscode job and handler

--- App/Queue/Handlers/SendPasscodeHandler.php
+++ App/Queue/Handlers/SendPasscodeHandler.php
@@ -8,7 +8,7 @@ use App\Logger;
 use App\Queue\Job\JobInterface;
 use App\Queue\Job\SendPasscode;
 use App\Queue\RabbitMQ\Dispatcher;
-use App\Repository\BookingRepositoryInterface;
+use App\Repository\LockRepositoryInterface;
 use App\Services\BookingService;
 
 class SendPasscodeHandler implements HandlerInterface
@@ -18,7 +18,7 @@ class SendPasscodeHandler implements HandlerInterface
 
     public function __construct(
         private readonly BookingService $bookingService,
-        private readonly BookingRepositoryInterface $bookingRepository,
+        private readonly LockRepositoryInterface $lockRepository,
         private readonly Dispatcher $dispatcher,
     ) {
     }
@@ -29,22 +29,17 @@ class SendPasscodeHandler implements HandlerInterface
     public function handle(JobInterface $job): void
     {
         try {
-            $bookingId = $job->getBookingId();
-            $booking = $this->bookingRepository->find($bookingId);
-            if (!$booking) {
-                Logger::error("Can't find Booking with Id: $bookingId");
+            $lockId = $job->getLockId();
+            $lock = $this->lockRepository->find($lockId);
+            if (!$lock) {
+                Logger::error("Can't find Lock by Id: $lockId");
                 return;
             }
 
-            if (!$booking->getLock()) {
-                Logger::error("Can't get the passcode from the Booking with Id: $bookingId");
-                return;
-            }
-
-            $this->bookingService->updateCode($booking);
-            Logger::log("Passcode {$booking->getLock()->getPasscode()} has been sent for {$booking->getName()}");
+            $this->bookingService->updateCode($lock);
+            Logger::log("Passcode {$lock->getPasscode()} has been sent for {$lock->getBooking()->getName()}");
         } catch (\Exception $e) {
-            $error = "Couldn't register new passcode for the booking id {$job->getBookingId()}. Error: {$e->getMessage()}.";
+            $error = "Couldn't register new passcode for the booking id {$job->getLockId()}. Error: {$e->getMessage()}.";
             if (++$job->attempts < self::ATTEMPTS_LIMIT) {
                 $error .= " The Job has been added to the queue. Attempt № $job->attempts";
                 $this->dispatcher->add($job, $job->attempts * self::DELAY);
